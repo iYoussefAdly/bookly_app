515e119849bf5cb7dfc941593d94591703cab12f
youssefadly <youssefadly332@gmail.com>
2025-07-17 15:09:35 +0300

    adding sliding animation to text of read free books

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slideAnimation,
+  });
+
+  final Animation<Offset> slideAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slideAnimation,
+      builder: (BuildContext context, Widget? child) {
+        return SlideTransition(
+          position: slideAnimation,
+          child: Text(
+            "Read free books",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,38 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+// Remove global variables and define them inside the state class
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +40,9 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo,width: 80,height: 80,),
-        Text("Read free Books",textAlign: TextAlign.center,)
+        Image.asset(AssetsData.logo, width: 80, height: 80),
+        SlidingText(slideAnimation: slideAnimation),
       ],
     );
   }
-} 
+}
