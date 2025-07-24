1da64a8de70ba097959b651db384dff5821987df
youssefadly <youssefadly332@gmail.com>
2025-07-24 15:01:09 +0300

    create SimilarBooksListView

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/features/home/presentation/views/widgets/book_rating_
 import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,32 +15,44 @@ class BookDetailsViewBody extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          SafeArea(child: CustomBookDetailsAppBar()),
-          SizedBox(height: 40,),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .18),
-            child: CustomBookImage(),
-          ),
-          SizedBox(height: 43),
-          Text(
-            "The Jungle Book",
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 6),
-          Opacity(
-            opacity: 0.8,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(color: kRomanSliverColor),
+      child: Expanded(
+        child: Column(
+          children: [
+            SafeArea(child: CustomBookDetailsAppBar()),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .22),
+              child: CustomBookImage(),
             ),
-          ),
-           SizedBox(height: 18),
-          BookRatingItem(mainAxisAlignment: MainAxisAlignment.center,),
-          SizedBox(height: 37,),
-          BooksAction()
-        ],
+            SizedBox(height: 33),
+            Text(
+              "The Jungle Book",
+              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 6),
+            Opacity(
+              opacity: 0.8,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(color: kRomanSliverColor),
+              ),
+            ),
+            SizedBox(height: 18),
+            BookRatingItem(mainAxisAlignment: MainAxisAlignment.center),
+            SizedBox(height: 37),
+            BooksAction(),
+            SizedBox(height: 50),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+              ),
+            ),
+            SizedBox(height: 16),
+            SimilarBooksListView(),
+            SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:flutter/cupertino.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*.16,
+      child: Expanded(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: CustomBookImage(),
+          ),
+        ),
+      ),
+    );;
+  }
+}
