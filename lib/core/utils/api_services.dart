a760e69bafb97707d607a61edd2d9b994c1f8928
youssefadly <youssefadly332@gmail.com>
2025-07-30 21:08:52 +0300

    implement fetchNewestBooks

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
 class ApiServices {
-  ApiServices(this.dio);
+  ApiServices(this._dio);
   final baseUrl = "https://www.googleapis.com/books/v1/";
-  final Dio dio;
+  final Dio _dio;
   Future<Map<String, dynamic>> get({required String endPoint}) async{
-    Response response = await dio.get("$baseUrl$endPoint");
+    Response response = await _dio.get("$baseUrl$endPoint");
     return response.data;
   }
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,30 @@
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImpl(this.apiServices);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+        endPoint:
+            "volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest",
+      );
+        List<BookModel> booksList = [];
+        for (var item in data["items"]) {
+          booksList.add(BookModel.fromJson(item));
+        }
+       return right(booksList);
+     
+    } on DioException catch (e) {
+      
+    }
   }
 
   @override
